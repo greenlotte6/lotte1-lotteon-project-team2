b53043278055e644ccd6227a1c4804eb35cc3d19
jaeyoung0560 <jaeyoung0560@gmail.com>
2025-04-24 15:49:32 +0900

    고객센터 공지사항 (카테고리x)

--- IntelliJ/lotteOn/src/main/java/kr/co/lotteon/service/article/CsService.java
+++ IntelliJ/lotteOn/src/main/java/kr/co/lotteon/service/article/CsService.java
@@ -2,10 +2,13 @@ package kr.co.lotteon.service.article;
 
 import com.querydsl.core.Tuple;
 import kr.co.lotteon.dto.article.InquiryDTO;
+import kr.co.lotteon.dto.article.NoticeDTO;
 import kr.co.lotteon.dto.page.PageRequestDTO;
 import kr.co.lotteon.dto.page.PageResponseDTO;
 import kr.co.lotteon.entity.article.Inquiry;
+import kr.co.lotteon.entity.article.Notice;
 import kr.co.lotteon.repository.article.InquiryRepository;
+import kr.co.lotteon.repository.article.NoticeRepository;
 import kr.co.lotteon.service.category.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +28,7 @@ public class CsService {
 
     private final InquiryRepository inquiryRepository;
     private final ModelMapper modelMapper;
+    private final NoticeRepository noticeRepository;
     private final CategoryService categoryService;
 
     public void register(InquiryDTO inquiryDTO) {
@@ -60,6 +64,33 @@ public class CsService {
         return PageResponseDTO.<InquiryDTO>builder()
                 .pageRequestDTO(pageRequestDTO)
                 .dtoList(inquiryDTOList)
+                .total(total)
+                .build();
+    }
+
+    public PageResponseDTO<NoticeDTO> noticeFindAll(PageRequestDTO pageRequestDTO, String cate) {
+
+        pageRequestDTO.setSize(10);
+        Pageable pageable = pageRequestDTO.getPageable("no");
+        Page<Notice> pageNotice;  // 변수명 변경
+
+        if(cate != null && !cate.isEmpty()){
+            // 카테고리로 검색
+            pageNotice = noticeRepository.findByCate(pageable, cate);
+        }else{
+            pageNotice = noticeRepository.findAll(pageable);
+        }
+
+        List<NoticeDTO> noticeDTOList = pageNotice.getContent().stream()
+                .map(notice -> modelMapper.map(notice, NoticeDTO.class))
+                .collect(Collectors.toList());
+
+        int total = (int) pageNotice.getTotalElements();
+
+
+        return PageResponseDTO.<NoticeDTO>builder()  // 제네릭 타입 변경
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(noticeDTOList)
                 .total(total)
                 .build();
     }
