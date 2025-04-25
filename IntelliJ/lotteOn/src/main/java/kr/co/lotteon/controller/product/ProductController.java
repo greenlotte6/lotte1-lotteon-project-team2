c95049f1db62aa561491309e807f37c09864971e
sohyeon <sohyoun26@gmail.com>
2025-04-25 17:48:41 +0900

    sohyeon

--- IntelliJ/lotteOn/src/main/java/kr/co/lotteon/controller/product/ProductController.java
+++ IntelliJ/lotteOn/src/main/java/kr/co/lotteon/controller/product/ProductController.java
@@ -1,31 +1,59 @@
 
 package kr.co.lotteon.controller.product;
 
+import kr.co.lotteon.dao.ProductMapper;
+import kr.co.lotteon.dto.coupon.CouponDTO;
 import kr.co.lotteon.dto.page.PageRequestDTO;
-import kr.co.lotteon.dto.page.PageViewDTO;
-import kr.co.lotteon.service.product.ProductService;
+import kr.co.lotteon.dto.page.PageResponseDTO;
+import kr.co.lotteon.dto.product.ProductDTO;
+import kr.co.lotteon.dto.product.ProductDetailDTO;
+import kr.co.lotteon.service.article.InquiryService;
+import kr.co.lotteon.service.coupon.CouponService;
+import kr.co.lotteon.service.feedback.ReviewService;
+import kr.co.lotteon.service.product.ProductDetailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ProductController {
 
-    private final ProductService productService;
+    private final ProductMapper productMapper;
+    private final ProductDetailService productDetailService;
+    private final ReviewService reviewService;
+    private final InquiryService inquiryService;
+    private final CouponService couponService;
 
     // 상세보기
     @GetMapping("/product/view")
-    public String productView(PageRequestDTO pageRequestDTO, Model model) {
+    public String view(PageRequestDTO pageRequestDTO, Model model) {
         pageRequestDTO.setSize(5);
-        PageViewDTO pageViewDTO = productService.getPageViewDTO(pageRequestDTO);
-        model.addAttribute("pageView", pageViewDTO);
+        String prodNo = pageRequestDTO.getProdNo();
+        // 상품 + 상품이미지
+        ProductDTO productDTO = productMapper.selectProductByProdNo(prodNo);
+        // 쿠폰
+        List<CouponDTO> couponDTOList = couponService.findAllByCompany(productDTO.getCompany());
+        // 상품 상세
+        ProductDetailDTO productDetailDTO = productDetailService.findByProdNo(pageRequestDTO);
+        // 리뷰
+        PageResponseDTO reviewPageResponseDTO = reviewService.selectAllForList(pageRequestDTO);
+        // qna
+        //PageResponseDTO inquiryPageResponseDTO = inquiryService.selectAllForList(pageRequestDTO);
+        model.addAttribute(productDTO);
+        model.addAttribute(couponDTOList);
+        model.addAttribute(productDetailDTO);
+        model.addAttribute("reviewPageResponseDTO", reviewPageResponseDTO);
 
+        log.info("reviewPageResponseDTO:{}", reviewPageResponseDTO);
         return "/product/beauty/perfume/view";
     }
+
 
     // 주문하기
     @GetMapping("/product/order")
--- IntelliJ/lotteOn/src/main/java/kr/co/lotteon/service/product/ProductService.java
+++ IntelliJ/lotteOn/src/main/java/kr/co/lotteon/service/product/ProductService.java
@@ -1,64 +1,19 @@
 package kr.co.lotteon.service.product;
 
 import jakarta.transaction.Transactional;
-import kr.co.lotteon.dao.ProductMapper;
-import kr.co.lotteon.dto.coupon.CouponDTO;
 import kr.co.lotteon.dto.page.PageRequestDTO;
-import kr.co.lotteon.dto.page.PageResponseDTO;
-import kr.co.lotteon.dto.page.PageViewDTO;
-import kr.co.lotteon.dto.product.ProductDTO;
-import kr.co.lotteon.dto.product.ProductDetailDTO;
-import kr.co.lotteon.entity.coupon.Coupon;
 import kr.co.lotteon.entity.product.Product;
 import kr.co.lotteon.repository.product.ProductRepository;
-import kr.co.lotteon.service.article.InquiryService;
-import kr.co.lotteon.service.coupon.CouponService;
-import kr.co.lotteon.service.feedback.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
-    private final ProductMapper productMapper;
     private final ProductRepository productRepository;
-    private final ProductDetailService productDetailService;
-    private final ReviewService reviewService;
-    private final InquiryService inquiryService;
-    private final CouponService couponService;
-    private final ModelMapper modelMapper;
-
-    // 상품 view 한번에 묶기
-    public PageViewDTO getPageViewDTO(PageRequestDTO pageRequestDTO) {
-        String prodNo = pageRequestDTO.getProdNo();
-        // 상품 + 상품이미지
-        ProductDTO productDTO = productMapper.selectProductByProdNo(prodNo);
-        // 쿠폰
-        List<CouponDTO> couponDTOList = couponService.findAllByCompany(productDTO.getCompany());
-        // 상품 상세
-        ProductDetailDTO productDetailDTO = productDetailService.findByProdNo(pageRequestDTO);
-        // 리뷰
-        PageResponseDTO reviewPageResponseDTO = reviewService.selectAllForList(pageRequestDTO);
-        //PageResponseDTO inquiryPageResponseDTO = inquiryService.selectAllForList(pageRequestDTO);
-
-        log.info("reviewPageResponseDTO: {}", reviewPageResponseDTO);
-
-        return new PageViewDTO(
-                productDTO,
-                couponDTOList,
-                productDetailDTO,
-                reviewPageResponseDTO
-                //inquiryPageResponseDTO
-        );
-    }
-
 
     // 글 조회수
     @Transactional
