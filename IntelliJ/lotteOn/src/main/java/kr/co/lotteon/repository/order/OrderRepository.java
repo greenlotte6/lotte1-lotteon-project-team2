7cff004228c7f054d75291450e4d5a710e03b655
jaeyeong <jaeyoung0560@gmail.com>
2025-04-29 17:50:41 +0900

    리스트 수정

---
+++ IntelliJ/lotteOn/src/main/java/kr/co/lotteon/repository/order/OrderRepository.java
@@ -0,0 +1,13 @@
+package kr.co.lotteon.repository.order;
+
+import kr.co.lotteon.entity.order.Order;
+import kr.co.lotteon.entity.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+    Page<Order> findAllByUser(User user, Pageable pageable);
+}
