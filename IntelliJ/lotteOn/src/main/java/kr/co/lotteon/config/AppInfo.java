19ca4730c516830336176a7e2b02f66cb488836e
GreenPai <clsrntkdgh@gmail.com>
2025-04-29 19:28:43 +0900

    환경설정

--- IntelliJ/lotteOn/src/main/java/kr/co/lotteon/config/AppInfo.java
+++ IntelliJ/lotteOn/src/main/java/kr/co/lotteon/config/AppInfo.java
@@ -33,9 +33,32 @@ public class AppInfo {
 
     // 데이터베이스의 버전정보
     private String appVersion;
+
+    // 사이트 정보
     private String title;
     private String subTitle;
     private String copyright;
+    private String headerLogo; //헤더 로고(브라우저 탭, 헤더 푸터 노출 로고)
+    private String footerLogo; //푸터 로고(브라우저 탭, 헤더 푸터 노출 로고)
+    private String favicon;    //파비콘
+
+    /*
+     * 기업 정보
+     * */
+    private String companyName; //상호명
+    private String ceoName; //대표이사
+    private String businessNumber; //사업자등록번호
+    private String onlineSalesNumber; //통신판매업신고번호
+    private String addr1; //기본주소
+    private String addr2; //상세주소
+
+    /*
+     * 고객센터 정보/카피라이트
+     * */
+    private String hp; //전화번호
+    private String workingHours; //업무시간
+    private String email; //이메일
+    private String efinDispute; //전자금융거래 분쟁담당
 
     // 처음 시작
     @PostConstruct
@@ -48,10 +71,33 @@ public class AppInfo {
             subTitle = config.getSubTitle();
             title = config.getTitle();
             copyright = config.getCopyright();
+            companyName = config.getCompanyName();
+            ceoName = config.getCeoName();
+            businessNumber = config.getBusinessNumber();
+            onlineSalesNumber = config.getOnlineSalesNumber();
+            addr1 = config.getAddr1();
+            addr2 = config.getAddr2();
+            hp = config.getHp();
+            workingHours = config.getWorkingHours();
+            email = config.getEmail();
+            efinDispute = config.getEfinDispute();
+            headerLogo = config.getHeaderLogo();
+            footerLogo = config.getFooterLogo();
+            favicon = config.getFavicon();
         }else{
             title = "롯데온";
             subTitle = "2조";
             copyright = "카피라이트";
+            companyName = "롯데쇼핑주식회사";
+            ceoName="홍길동";
+            businessNumber = "123-12-12345";
+            onlineSalesNumber = "2024-서울강남-1234";
+            addr1 = "서울특별시강남구테헤란로";
+            addr2 = "(삼성동 , WeWork 빌딩) 7층";
+            hp = "02-1234-5678";
+            workingHours = "평일 09:00 ~ 18:00";
+            email = "lotteon@lotte.net";
+            efinDispute = "1234-1234";
         }
 
         if(version == null){
@@ -77,10 +123,33 @@ public class AppInfo {
             subTitle = config.getSubTitle();
             title = config.getTitle();
             copyright = config.getCopyright();
+            companyName = config.getCompanyName();
+            ceoName = config.getCeoName();
+            businessNumber = config.getBusinessNumber();
+            onlineSalesNumber = config.getOnlineSalesNumber();
+            addr1 = config.getAddr1();
+            addr2 = config.getAddr2();
+            hp = config.getHp();
+            workingHours = config.getWorkingHours();
+            email = config.getEmail();
+            efinDispute = config.getEfinDispute();
+            headerLogo = config.getHeaderLogo();
+            footerLogo = config.getFooterLogo();
+            favicon = config.getFavicon();
         }else{
             title = "롯데온";
             subTitle = "2조";
             copyright = "카피라이트";
+            companyName = "롯데쇼핑주식회사";
+            ceoName="홍길동";
+            businessNumber = "123-12-12345";
+            onlineSalesNumber = "2024-서울강남-1234";
+            addr1 = "서울특별시강남구테헤란로";
+            addr2 = "(삼성동 , WeWork 빌딩) 7층";
+            hp = "02-1234-5678";
+            workingHours = "평일 09:00 ~ 18:00";
+            email = "lotteon@lotte.net";
+            efinDispute = "1234-1234";
         }
     }
 
--- IntelliJ/lotteOn/src/main/java/kr/co/lotteon/config/WebConfig.java
+++ IntelliJ/lotteOn/src/main/java/kr/co/lotteon/config/WebConfig.java
@@ -5,21 +5,20 @@ import kr.co.lotteon.interceptor.AppInfoInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-
-/*
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 기본 static 경로 설정 (예: /static/ 경로로 접근할 수 있도록 설정)
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations("file:uploads/");
     }
-*/
+
 
     private final AppInfo appInfo;
 
