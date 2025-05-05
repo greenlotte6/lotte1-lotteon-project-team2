5d7a42c85babb245652c6aebd412cadffa31debd
roberty <clsrntkdgh@gmail.com>
2025-05-06 03:53:47 +0900

    주문 페이지 초안

--- IntelliJ/lotteOn/src/main/java/kr/co/lotteon/controller/order/OrderController.java
+++ IntelliJ/lotteOn/src/main/java/kr/co/lotteon/controller/order/OrderController.java
@@ -2,35 +2,92 @@ package kr.co.lotteon.controller.order;
 
 
 import kr.co.lotteon.dto.order.OrderDTO;
-import kr.co.lotteon.dto.order.OrderItemDTO;
+import kr.co.lotteon.service.order.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class OrderController {
 
+    private final OrderService orderService;
+
+    /*
+    * 1. 주문
+    * Order 테이블에 데이터 넣기
+    * OrderDetail 테이블에 데이터 넣기
+    * */
+
+    /*
+    * 2. 포인트
+    * Point 테이블에 사용한 포인트량, 적립된 포인트량 기록
+    * UserDetail 테이블에 포인트 총량 기록
+    * */
+
+    /*
+    * 3. 상품
+    * Product 테이블에 재고(prodStock), 판매량(prodSold) 계산하기
+    * */
+    
+    /*
+    * 4. 쿠폰
+    * 사용한 쿠폰 상태 바꾸기
+    * */
+    
+    /*
+    * 4. 장바구니
+    * 구매한 장바구니 cartNo 지우기
+    * */
+
+
+    /*
+    * 진행한거 :
+    * 장바구니 페이지 체크 안됬을 때 생기는 에러 해결
+    * 결제 페이지 버퍼링 추가
+    * Order 테이블 데이터 넣기
+    * OrderItem 테이블 데이터 넣기 
+    * */
+
     @PostMapping("/order/submit")
     public String orderSubmit(OrderDTO orderDTO,
                               @RequestParam int point,
-                              @RequestParam int cartNo,
+                              @RequestParam("cartNo") List<Integer> cartNos,
                               @RequestParam String receiverAddr1,
                               @RequestParam String receiverAddr2,
-                              @RequestParam(value = "couponNo", required = false) String couponNo) {
+                              @RequestParam(value = "couponNo", required = false) String couponNo,
+                              @AuthenticationPrincipal UserDetails userDetails) {
 
+        orderDTO.setUid(userDetails.getUsername());
+        orderDTO.setOrderAddr(receiverAddr1 + " " + receiverAddr2);
+
+        /*
+        * orderStatus, orderDate -> 둘 다 자동등록 따로 설정 x
+        * orderSender=null, senderHp=null -> User 조회해서 이름, hp 넣어줄 예정
+        * */
+
+        // Order 테이블 등록하기 -> 등록 후 orderItem을 등록하기 위한 order 출력
+        int orderNo = orderService.registerOrder(orderDTO);
+
+        // 상세 주문 등록하기
+        orderService.registerOrderItem(orderNo, cartNos);
+        
         log.info("orderDTO = {}", orderDTO);
         log.info("point = {}", point);
-        log.info("cartNo = {}", cartNo);
+        log.info("cartNo = {}", cartNos);
         log.info("couponNo = {}", couponNo);
         log.info("recipientAddr1 = {}", receiverAddr1);
         log.info("recipientAddr2 = {}", receiverAddr2);
 
 
-        return null;
+        return "redirect:/";
     }
 
 }
--- IntelliJ/lotteOn/src/main/java/kr/co/lotteon/service/order/OrderService.java
+++ IntelliJ/lotteOn/src/main/java/kr/co/lotteon/service/order/OrderService.java
@@ -1,12 +1,19 @@
 package kr.co.lotteon.service.order;
 
 import kr.co.lotteon.dto.cart.CartDTO;
+import kr.co.lotteon.dto.order.OrderDTO;
 import kr.co.lotteon.dto.page.ItemRequestDTO;
 import kr.co.lotteon.dto.product.ProductDTO;
 import kr.co.lotteon.entity.cart.Cart;
+import kr.co.lotteon.entity.order.Order;
+import kr.co.lotteon.entity.order.OrderItem;
 import kr.co.lotteon.entity.product.Product;
+import kr.co.lotteon.entity.user.User;
+import kr.co.lotteon.repository.order.OrderItemRepository;
+import kr.co.lotteon.repository.order.OrderRepository;
 import kr.co.lotteon.repository.product.CartRepository;
 import kr.co.lotteon.repository.product.ProductRepository;
+import kr.co.lotteon.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -22,6 +29,9 @@ import java.util.Optional;
 public class OrderService {
 
     private final CartRepository cartRepository;
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final UserRepository userRepository;
     private final ModelMapper modelMapper;
 
     public CartDTO findByCartNo(int cartNo) {
@@ -35,4 +45,59 @@ public class OrderService {
         return null;
     }
 
+    /*
+    * 주문 등록
+    * */
+    public int registerOrder(OrderDTO orderDTO) {
+        String uid =  orderDTO.getUid();
+        Optional<User> userOpt = userRepository.findByUid(uid);
+
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            orderDTO.setOrderSender(user.getName());
+            orderDTO.setSenderHp(user.getHp());
+
+            // 카드 번호(임시)
+            String paymentContent = "1234 4567 7894 1234";
+            orderDTO.setPaymentContent(paymentContent);
+
+            Order order = modelMapper.map(orderDTO, Order.class);
+            order.setUser(user);
+
+            Order saveOrder = orderRepository.save(order);
+            return saveOrder.getOrderNo();
+
+        }
+
+        return 0;
+
+    }
+
+    public void registerOrderItem(int orderNo, List<Integer> cartNos) {
+        for (Integer cartNo : cartNos) {
+            Cart cart = cartRepository.findById(cartNo).get();
+            Product product = cart.getProduct();
+
+            Order order = Order.builder()
+                    .orderNo(orderNo)
+                    .build();
+
+            OrderItem orderItem = OrderItem.builder()
+                    .order(order)
+                    .product(product)
+                    .opt1(cart.getOpt1()).opt2(cart.getOpt2())
+                    .opt3(cart.getOpt3()).opt4(cart.getOpt4())
+                    .opt5(cart.getOpt5()).opt6(cart.getOpt6())
+                    .opt1Cont(cart.getOpt1Cont()).opt2Cont(cart.getOpt2Cont())
+                    .opt3Cont(cart.getOpt3Cont()).opt4Cont(cart.getOpt4Cont())
+                    .opt5Cont(cart.getOpt5Cont()).opt6Cont(cart.getOpt6Cont())
+                    .itemCount(cart.getCartProdCount())
+                    .itemPrice(product.getProdPrice())
+                    .itemDiscount(product.getProdDiscount())
+                    .build();
+
+            orderItemRepository.save(orderItem);
+
+        }
+    }
 }
